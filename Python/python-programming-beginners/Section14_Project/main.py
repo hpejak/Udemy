c0d26cf9521bfc6437a657c38cd65e6ad8bea3a9
hpejak <pejak.hrvoje@gmail.com>
2020-01-05 20:09:04 +0100

    Finishing Project for Udemy class "Programing with Python: HandOn Introduction for eginners"

--- Python/python-programming-beginners/Section14_Project/main.py
+++ Python/python-programming-beginners/Section14_Project/main.py
@@ -1,38 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import functions
 
 students = {"John Wick":780, "John McClane":870,"Martin Riggs":920,"Roger Murtaugh":540,"Stanley Goodspeed":480,"John Patrick Mason":760,"Francis X. Hummel":660,"Cameron Poe":970,"Vince Larkin":580,"Castor Troy":770,"Sean Archer":1000,
 "Randall Raines":980,"Sara Wayland":710,"Max Rockatansky":950}
 
 rewards = (500,300,100)
 
-def firstThreeStudents(students):
-    firstStudent = None
-    secondStudent = None
-    thirdStudent = None
 
-    for student in students:
-        if not firstStudent:
-            firstStudent = student
-        if not secondStudent:
-            secondStudent = student
-        if not thirdStudent:
-            thirdStudent = student
+addStudents = input("Do you won't to add new Student? (yes/NO): ")
 
-        if students[student] > students[firstStudent]:
-            if students[firstStudent] > students[secondStudent]:
-                if students[secondStudent] > students[thirdStudent]:
-                    thirdStudent = secondStudent
-                secondStudent = firstStudent
-            firstStudent = student
-        elif students[student] > students[secondStudent]:
-            if students[secondStudent] > students[thirdStudent]:
-                thirdStudent = secondStudent
-            secondStudent = student
-        elif students[student] > students[thirdStudent]:
-            thirdStudent = student
-
-    print firstStudent,secondStudent,thirdStudent
+if (addStudents.lower() == "yes"):
+    functions.insertStudents(students)
 
 
-firstThreeStudents(students)
+functions.appreciateStudent(students)
+
+functions.reward(functions.topStudents(students),rewards)
